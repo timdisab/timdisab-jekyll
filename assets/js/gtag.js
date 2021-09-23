9eb67e53755d2ed9d25cb951134d0e74d6084d44
Tim DiSabatino <tim@pathwaysdigital.com>
2021-09-23 05:31:31 -0700

    Embed GA directly

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,9 +1,1 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99736283-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-99736283-1');
-</script>
+// Can put GA here if you want
